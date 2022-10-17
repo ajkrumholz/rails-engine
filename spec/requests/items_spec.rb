44a0b7e24ecba574ed9222c1fda45827fba5cca5
A.J. Krumholz <krumholz.alan@gmail.com>
2022-10-17 16:56:49 -0400

    add tests for items index

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Items", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:items) { create_list(:items, 10) }
+
+  describe "items index" do
+    before { get "/api/v1/items" }
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns all items' do
+      expect(json).to_not be_empty
+      expect(json[:data].count).to eq(10)
+    end
+
+    it 'returns attributes for each item' do
+      items.each_with_index do |item, i|
+        subject = json[:data][i]
+        expect(subject[:id]).to eq(item.id.to_s)
+        expect(subject[:type).to eq('item')
+        expect(subject[:attributes][:name]).to eq(item.name)
+        expect(subject[:attributes][:description]).to eq(item.description)
+        expect(subject[:attributes][:unit_price]).to eq(item.unit_price)
+      end
+    end
   end
 end
