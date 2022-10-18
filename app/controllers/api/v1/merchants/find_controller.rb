565b2d2524d2f948aacde10cab1bd5ccb2157352
A.J. Krumholz <krumholz.alan@gmail.com>
2022-10-18 19:21:48 -0400

    feat: merchant find all returns all names matching a fragment

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -2,9 +2,13 @@ module Api
   module V1
     module Merchants
       class FindController < ApplicationController
-        def show
-          found_merchant = Merchant.where("name ILIKE ?", "%#{params[:name]}%").order(name: :desc).first
+        def index
+          found_merchants = Merchant.where("name ILIKE ?", "%#{params[:name]}%").order(:name)
+          render json: MerchantSerializer.new(found_merchants)
+        end
 
+        def show
+          found_merchant = Merchant.where("name ILIKE ?", "%#{params[:name]}%").order(:name).first
           render json: MerchantSerializer.new(found_merchant)
         end
       end
--- spec/requests/merchants/find_spec.rb
+++ spec/requests/merchants/find_spec.rb
@@ -1,13 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe 'get merchants/find' do
-  let!(:shrimp) { create :merchant, name: "Red Shrimp" }
-  let!(:duck) { create :merchant, name: "Blue Duck" }
-  let!(:badger) { create :merchant, name: "Hardy Badger" }
+RSpec.describe 'Merchant searches' do
+  describe 'get merchant/find' do
+    let!(:shrimp) { create :merchant, name: "Red Shrimp" }
+    
+    it 'returns a merchant by matching a fragment of their name' do
+      get '/api/v1/merchants/find?name=Red'
+      expect(response).to have_http_status(200)
+      expect(json[:data][:attributes][:name]).to eq(shrimp.name)
+    end
 
-  it 'returns a merchant by matching a fragment of their name' do
-    get '/api/v1/merchants/find?name=Red'
-    expect(response).to have_http_status(200)
-    expect(json[:data][:attributes][:name]).to eq(shrimp.name)
+    it 'returns merchant when case doesnt match' do
+      get '/api/v1/merchants/find?name=red'
+      expect(json[:data][:attributes][:name]).to eq(shrimp.name)
+    end
+
+    it 'when more than one merchant matches, returns first alpha' do
+      blue = create(:merchant, name: "Blue Shrimp")
+      get '/api/v1/merchants/find?name=Shrimp'
+      expect(json[:data][:attributes][:name]).to eq(blue.name)
+    end
+  end
+
+  describe 'get merchant/find_all' do
+    let!(:aardvark) { create :merchant, name: 'Aardvark Waffles' }
+    let!(:gorilla) { create :merchant, name: 'Gorilla Waffles' }
+    let!(:elephant) { create :merchant, name: 'Elephant Waffles' }
+    let!(:giraffe) { create :merchant, name: 'Giraffe Pancakes'}
+
+    it 'returns all merchants matching the fragment' do
+      get '/api/v1/merchants/find_all?name=waffles'
+      name_array = json[:data].map { |merchant| merchant[:attributes][:name] }
+      expect(name_array).to include(aardvark.name, gorilla.name, elephant.name)
+      expect(name_array).not_to include(giraffe.name)
+    end
   end
 end
