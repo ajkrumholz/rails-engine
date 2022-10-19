989f4c47399d3730988d51f17b9e04bfa29976fe
A.J. Krumholz <krumholz.alan@gmail.com>
2022-10-19 09:59:43 -0400

    remove comments

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -1,12 +1,6 @@
 class ErrorSerializer
 
   def self.no_merchant
-    # { 
-    #   message: "Could not complete query",
-    #   error: [
-    #     "Merchant could not be located"
-    #   ]
-    # }
     handle_error("Merchant could not be located")
   end
 
