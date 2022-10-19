4cb157e98b888eec77d734223600d4e680e13245
A.J. Krumholz <krumholz.alan@gmail.com>
2022-10-19 15:13:34 -0400

    move query methods to Item.model

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -2,96 +2,149 @@ module Api
   module V1
     module Items
       class FindController < ApplicationController
+        before_action :set_vars
+                
         def index
-          
-
-          if params[:name].present?
-            if params[:min_price].present? || params[:max_price].present?
-              render json: ErrorSerializer.invalid_search, status: 400 and return
-            else
-              result = Item.where("name ILIKE ?", "%#{params[:name]}%").order(:name)
-            end
-          elsif params[:min_price].present? && !params[:max_price].present?
-            if params[:min_price] < "0"
-              render json: ErrorSerializer.negative_price, status: 400 and return
-            else
-              result = Item.where("unit_price >= ?", params[:min_price])
-            end
-          elsif !params[:min_price].present? && params[:max_price].present?
-            if params[:max_price] < "0"
-              render json: ErrorSerializer.negative_price, status: 400 and return
-            else
-              result = Item.where("unit_price <= ?", params[:max_price])
-            end
-          elsif params[:min_price].present? && params[:max_price].present?
-            if params[:min_price] < "0" || params[:max_price] < "0"
-              render json: ErrorSerializer.negative_price, status: 400 and return
-            elsif params[:min_price].to_i > params[:max_price].to_i
-              render json: ErrorSerializer.min_greater, status: 400 and return
-            else
-              result = Item.where("unit_price between ? and ?", params[:min_price], params[:max_price])
-            end
-          else
-            render json: ErrorSerializer.missing_parameter and return
+          validate_params
+          if name_query
+            result = Item.name_query(@name)
+          elsif min_query
+            result = Item.min_query(@min_price)
+          elsif max_query
+            result = Item.max_query(@max_price)
+          elsif range_query
+            result = Item.range_query(@min_price, @max_price)
           end
+          render json: ItemSerializer.new(result)
+        end
 
-          if result.empty?
-            render json: ErrorSerializer.no_item
-          else
-            render json: ItemSerializer.new(result)
+        private
+
+        def set_vars
+          @name = params[:name]
+          @min_price = params[:min_price]
+          @max_price = params[:max_price]
+        end
+
+        def validate_params
+          name_and_price?
+          negative_price?(@min_price)
+          negative_price?(@max_price)
+          min_greater
+          missing_query
+          blank_query
+        end
+
+        def name_and_price?
+          if @name.present? && (@min_price.present? || @max_price.present?)
+            render json: ErrorSerializer.invalid_search, status: 400
           end
         end
 
+        def negative_price?(price)
+          if price.present? && price < "0"
+            render json: ErrorSerializer.negative_price, status: 400
+          end
+        end
 
+        def min_greater
+          if @min_price.present? && @max_price.present?
+            if @min_price.to_i > @max_price.to_i
+              render json: ErrorSerializer.min_greater, status: 400
+            end
+          end
+        end
 
+        def missing_query
+          if !@name && !@min_price && !@max_price
+            render json: ErrorSerializer.missing_parameter, status: 400
+          end
+        end
 
-        #   if !params[:name].present?
-        #     if !params[:max_price].present? && !params[:min_price].present?
-        #     render json: ErrorSerializer.missing_parameter
-        #     else
-        #       if params[:max_price].present? && !params[:min_price].present?
-        #         if params[:max_price] < "0"
-        #           render json: ErrorSerializer.negative_price, status: 400 and return
-        #         else
-        #           result = Item.where("unit_price <= ?", params[:max_price])
-        #         end
-        #       elsif !params[:max_price].present? && params[:min_price].present?
-        #         if params[:min_price] < "0"
-        #           render json: ErrorSerializer.negative_price, status: 400 and return
-        #         else
-        #           result = Item.where("unit_price >= ?", params[:min_price])
-        #         end
-        #       else
-        #         if params[:min_price] < "0" || params[:max_price] < "0"
-        #           render json: ErrorSerializer.negative_price, status: 400 and return
-        #         elsif params[:min_price].to_i > params[:max_price].to_i
-        #           render json: ErrorSerializer.min_greater, status: 400 and return
-        #         else
-        #           result = Item.where("unit_price between ? and ?", params[:min_price], params[:max_price])
-        #         end
-        #       end
-        #       if result.empty?
-        #         render json: ErrorSerializer.no_item
-        #       else
-        #         render json: ItemSerializer.new(result)
-        #       end
-        #     end
-        #   elsif params[:max_price].present? || params[:min_price].present?
-        #     render json: ErrorSerializer.invalid_search, status: 400 and return 
-        #   else
-        #     result = Item.where("name ILIKE ?", "%#{params[:name]}%").order(:name)
-        #     if result.empty?
-        #       render json: ErrorSerializer.no_item
-        #     else
-        #       render json: ItemSerializer.new(result)
-        #     end
-        #   end
-        # end
+        def blank_query
+          if @name.blank? || @min_price.blank? || @max_price.blank?
+            render json: ErrorSerializer.missing_parameter, status: 400
+          end
+        end
 
-        def show
+        def name_query
+          @name.present?
+        end
 
+        def min_query
+          @min_price.present? && !@max_price.present?
+        end
+
+        def max_query
+          @max_price.present? && !@min_price.present?
+        end
+
+        def range_query
+          @min_price.present? && @max_price.present?
         end
       end
     end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #   if params[:name].present?
+        #     if params[:min_price].present? || params[:max_price].present?
+        #       render json: ErrorSerializer.invalid_search, status: 400 and return
+        #     else
+        #       result = Item.where("name ILIKE ?", "%#{params[:name]}%").order(:name)
+        #     end
+        #   elsif params[:min_price].present? && !params[:max_price].present?
+        #     if params[:min_price] < "0"
+        #       render json: ErrorSerializer.negative_price, status: 400 and return
+        #     else
+        #       result = Item.where("unit_price >= ?", params[:min_price])
+        #     end
+        #   elsif !params[:min_price].present? && params[:max_price].present?
+        #     if params[:max_price] < "0"
+        #       render json: ErrorSerializer.negative_price, status: 400 and return
+        #     else
+        #       result = Item.where("unit_price <= ?", params[:max_price])
+        #     end
+        #   elsif params[:min_price].present? && params[:max_price].present?
+        #     require 'pry'; binding.pry
+        #     if params[:min_price] < "0" || params[:max_price] < "0"
+        #       render json: ErrorSerializer.negative_price, status: 400 and return
+        #     elsif params[:min_price].to_i > params[:max_price].to_i
+        #       render json: ErrorSerializer.min_greater, status: 400 and return
+        #     else
+        #       result = Item.where("unit_price between ? and ?", params[:min_price], params[:max_price])
+        #     end
+        #   else
+        #     render json: ErrorSerializer.missing_parameter and return
+        #   end
+        #   if result.empty?
+        #     render json: ErrorSerializer.no_item
+        #   else
+        #     render json: ItemSerializer.new(result)
+        #   end
+        # end
+
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,7 +12,70 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of :description }
     it { should validate_presence_of :unit_price }
     it { should validate_numericality_of(:unit_price).is_greater_than(0) }
-    # it { should allow_value(9.99).for(:unit_price) }
-    # it { should_not allow_value(9.999).for(:unit_price) }
+  end
+
+  describe 'class methods' do
+    describe 'search methods' do
+      let!(:merchant) { create :merchant }
+      let!(:other_merchant) { create :merchant }
+      
+      describe '::name_query' do
+        let!(:item_1) { create :item, name: "angry albatross", merchant: merchant }
+        let!(:item_2) { create :item, name: "sleuthy seagull", merchant: merchant }
+        let!(:item_3) { create :item, name: "loopy albatross", merchant: other_merchant }
+        let!(:item_4) { create :item, name: "jessica albatross", merchant: merchant }
+
+        it 'returns all items matching a fragment of name' do
+          query = Item.name_query('alba') 
+          expect(query.first).to be_an(Item)
+          expect(query).to include(item_1, item_3, item_4)
+          expect(query).not_to include(item_2)
+        end
+
+        it 'returns matches ignoring case' do
+          query = Item.name_query('AlBa') 
+
+          expect(query).to include(item_1, item_3, item_4)
+          expect(query).not_to include(item_2)
+        end
+
+        it 'orders results alphabetically' do
+          query = Item.name_query('alba') 
+          
+          expect(query).to eq([item_1, item_4, item_3])
+        end
+      end
+
+      describe 'price_queries' do
+        let!(:item_1) { create :item, unit_price: 4.99, merchant: merchant }
+        let!(:item_2) { create :item, unit_price: 9.99, merchant: merchant }
+        let!(:item_3) { create :item, unit_price: 14.99, merchant: other_merchant }
+        let!(:item_4) { create :item, unit_price: 20.99, merchant: merchant }
+      
+        describe '::min_query' do
+          it 'returns items with unit_price >= min_price' do
+            query = Item.min_query(5.00)
+            expect(query).to include(item_2, item_3, item_4)
+            expect(query).not_to include(item_1)
+          end
+        end
+
+        describe '::max_query' do
+          it 'returns items with unit_price <= to max_price' do
+            query = Item.max_query(20.00)
+            expect(query).to include(item_1, item_2, item_3)
+            expect(query).not_to include(item_4)
+          end
+        end
+
+        describe '::range_query' do
+          it 'returns items with unit_price <= max and >= min' do
+            query = Item.range_query(5.00, 20.00)
+            expect(query).to include(item_2, item_3)
+            expect(query).not_to include(item_1, item_4)
+          end
+        end
+      end
+    end
   end
 end
